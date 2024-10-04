ee60d7e2f8eeb773116aee92a6d04f079d07e28f
Jason <jason@strivemath.com>
2024-10-04 18:29:27 +0200

    use p5 builtins

--- src/lib/p5.js
+++ src/lib/p5.js
@@ -1,26 +1,16 @@
 function $builtinmodule() {
-    const {
-        builtins: __builtins__,
-        builtin: {
-            str: pyStr,
-            func: pyFunc,
-            none: { none$: pyNone },
-        },
-        misceval: { callsimArray: pyCall },
-        ffi: { toPy },
-    } = Sk;
 
     const mod = {
-        __name__: new pyStr("p5"),
-        p5: toPy(window.p5),
-        __doc__: new pyStr("A skulpt implementation of the p5 library"),
+        __name__: new Sk.builtin.str("p5"),
+        p5: Sk.ffi.remapToPy(window.p5),
+        __doc__: new Sk.builtin.str("A skulpt implementation of the p5 library"),
     };
 
     // override _start to a plain function and call this in run when we need it
     let _start;
     Object.defineProperty(window.p5.prototype, "_start", {
         get() {
-            return () => { };
+            return () => {};
         },
         set(val) {
             _start = val;
@@ -30,54 +20,72 @@ function $builtinmodule() {
 
     function sketch(p) {
         for (let i in window.p5.prototype) {
-            const asStr = new pyStr(i);
+            const asStr = new Sk.builtin.str(i);
             const mangled = asStr.$mangled;
             // it would be crazy to override builtins like print
-            if (!(mangled in __builtins__)) {
+            if (!(mangled in Sk.builtins)) {
                 mod[mangled] = p.tp$getattr(asStr);
             }
         }
     }
 
     // create an instance of p5 and assign all the attributes to mod
-    const p = pyCall(mod.p5, [new pyFunc(sketch), toPy(Sk.canvas)]);
+    const p = Sk.misceval.callsimArray(mod.p5, [new Sk.builtin.func(sketch), Sk.ffi.remapToPy(Sk.canvas)]);
 
     const wrapFunc = (func) => () => {
         try {
-            pyCall(func);
+            Sk.misceval.callsimArray(func);
         } catch (e) {
             Sk.uncaughtException && Sk.uncaughtException(e);
         }
-        // note we can't suspend because promises are just ignored in these methods
+    // note we can't suspend because promises are just ignored in these methods
     };
 
-    mod.run = new pyFunc(function run() {
-        const main = Sk.sysmodules.quick$lookup(new pyStr("__main__")).$d;
+    mod.run = new Sk.builtin.func(function run() {
+        const main = Sk.sysmodules.quick$lookup(new Sk.builtin.str("__main__")).$d;
         delete window.p5.prototype._start;
         const pInstance = p.valueOf();
         pInstance._start = _start;
 
-        ["preload", "setup", "draw", "deviceMoved", "deviceTurned", "deviceShaken",
-            "windowResized", "keyPressed", "keyReleased", "keyTyped", "mousePressed",
-            "mouseReleased", "mouseClicked", "doubleClicked", "mouseMoved", "mouseDragged",
-            "mouseWheel", "touchStarted", "touchMoved", "touchEnded"].forEach((methodName) => {
-                const method = main[methodName];
-                if (method !== undefined) {
-                    pInstance[methodName] = wrapFunc(method);
-                }
-            });
+        [
+            "preload",
+            "setup",
+            "draw",
+            "deviceMoved",
+            "deviceTurned",
+            "deviceShaken",
+            "windowResized",
+            "keyPressed",
+            "keyReleased",
+            "keyTyped",
+            "mousePressed",
+            "mouseReleased",
+            "mouseClicked",
+            "doubleClicked",
+            "mouseMoved",
+            "mouseDragged",
+            "mouseWheel",
+            "touchStarted",
+            "touchMoved",
+            "touchEnded",
+        ].forEach((methodName) => {
+            const method = main[methodName];
+            if (method !== undefined) {
+                pInstance[methodName] = wrapFunc(method);
+            }
+        });
 
         // p5 wants to change the global namespace of things like frameCount, key. So let it
         const _setProperty = pInstance._setProperty;
         pInstance._setProperty = function (prop, val) {
             _setProperty.call(this, prop, val);
-            const asStr = new pyStr(prop);
+            const asStr = new Sk.builtin.str(prop);
             const mangled = asStr.$mangled;
             mod[mangled] = main[mangled] = p.tp$getattr(asStr);
         };
 
         pInstance._start();
-        return pyNone;
+        return Sk.builtin.none.none$;
     });
 
     return mod;
